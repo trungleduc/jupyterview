7bc54d196a9f98666ebb63199b884dd789b0f278
LE Duc Trung <leductrungxf@gmail.com>
2020-04-06 11:11:27 +0200

    Add axis to scene

--- src/vtk_components/vtkwidget.tsx
+++ src/vtk_components/vtkwidget.tsx
@@ -22,6 +22,23 @@ import {
 } from 'vtk.js/Sources/Rendering/Core/Mapper/Constants';
 //@ts-ignore
 import vtkColorMaps from 'vtk.js/Sources/Rendering/Core/ColorTransferFunction/ColorMaps';
+//@ts-ignore
+import vtkOrientationMarkerWidget from 'vtk.js/Sources/Interaction/Widgets/OrientationMarkerWidget';
+//@ts-ignore
+import vtkAxesActor from 'vtk.js/Sources/Rendering/Core/AxesActor';
+//@ts-ignore
+import vtkInteractiveOrientationWidget from 'vtk.js/Sources/Widgets/Widgets3D/InteractiveOrientationWidget';
+//@ts-ignore
+import vtkWidgetManager from 'vtk.js/Sources/Widgets/Core/WidgetManager';
+//@ts-ignore
+import * as vtkMath from 'vtk.js/Sources/Common/Core/Math';
+function majorAxis(vec3 : Array<number>, idxA: number, idxB : number) {
+  const axis = [0, 0, 0];
+  const idx = Math.abs(vec3[idxA]) > Math.abs(vec3[idxB]) ? idxA : idxB;
+  const value = vec3[idx] > 0 ? 1 : -1;
+  axis[idx] = value;
+  return axis;
+}
 
 
 export default class VtkWidget extends React.Component<{}, {colorOption : any}> {
@@ -176,7 +193,63 @@ export default class VtkWidget extends React.Component<{}, {colorOption : any}> 
       this.renderer = this.fullScreenRenderer.getRenderer();
       this.renderWindow = this.fullScreenRenderer.getRenderWindow();
   
+      const axes = vtkAxesActor.newInstance();
+      const orientationWidget = vtkOrientationMarkerWidget.newInstance({
+        actor: axes,
+        interactor: this.renderWindow.getInteractor(),
+      });
+      orientationWidget.setEnabled(true);
+      orientationWidget.setViewportCorner(
+        vtkOrientationMarkerWidget.Corners.BOTTOM_LEFT
+      );
+      orientationWidget.setViewportSize(0.15);
+      orientationWidget.setMinPixelSize(100);
+      orientationWidget.setMaxPixelSize(300);
+      
+      const camera = this.renderer.getActiveCamera();
+      const widgetManager = vtkWidgetManager.newInstance();
+      widgetManager.setRenderer(orientationWidget.getRenderer());
+      
+      const widget = vtkInteractiveOrientationWidget.newInstance();
+      widget.placeWidget(axes.getBounds());
+      widget.setBounds(axes.getBounds());
+      widget.setPlaceFactor(1);
+      
+      const vw = widgetManager.addWidget(widget);
+      
+      // Manage user interaction
+      vw.onOrientationChange(({ up , direction, action, event }: any) => {
+        const focalPoint = camera.getFocalPoint();
+        const position = camera.getPosition();
+        const viewUp = camera.getViewUp();
+      
+        const distance = Math.sqrt(
+          vtkMath.distance2BetweenPoints(position, focalPoint)
+        );
+        camera.setPosition(
+          focalPoint[0] + direction[0] * distance,
+          focalPoint[1] + direction[1] * distance,
+          focalPoint[2] + direction[2] * distance
+        );
+      
+        if (direction[0]) {
+          camera.setViewUp(majorAxis(viewUp, 1, 2));
+        }
+        if (direction[1]) {
+          camera.setViewUp(majorAxis(viewUp, 0, 2));
+        }
+        if (direction[2]) {
+          camera.setViewUp(majorAxis(viewUp, 0, 1));
+        }
+      
+        orientationWidget.updateMarkerOrientation();
+        widgetManager.enablePicking();
+        this.renderWindow.render();
+      });
+
+
       this.renderer.resetCamera();
+      widgetManager.enablePicking();
       this.renderWindow.render();
 
     }, 500);
