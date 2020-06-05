2002af9ca9656e7929d6d156842aa2cf9f84e137
LE Duc Trung <leductrungxf@gmail.com>
2020-06-05 23:08:38 +0200

    Add base url  to itk config

--- src/itkConfigJupyter.ts
+++ src/itkConfigJupyter.ts
@@ -1,7 +1,8 @@
 // Copyright (c) InsightSoftwareConsortium
 // Distributed under the terms of the Apache License 2.0.
-
-let _public_path__ = "/nbextensions/jupyterview/";
+import { PageConfig } from "./tools/getOption";
+const basePath = PageConfig.getOption("baseUrl");
+let _public_path__ = basePath + "nbextensions/jupyterview/";
 const itkConfig = {
   itkModulesPath: _public_path__ + "itk",
 };
