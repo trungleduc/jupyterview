681f607f8ab3b3b8bca3ec3373b21ae4162c19ea
Duc Trung LE <leductrungxf@gmail.com>
2022-01-22 16:50:12 +0100

    Include itk wasm files

---
+++ extension.webpack.config.js
@@ -0,0 +1,94 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: 'javascript/auto',
+        loader: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js'],
+    alias: {
+      './itkConfig$': path.resolve(__dirname, 'lib', 'itkConfigJupyter.js')
+    }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, 'node_modules', 'itk', 'WebWorkers'),
+          to: path.join(
+            __dirname,
+            'jupyterview',
+            'labextension',
+            'static',
+            'itk',
+            'WebWorkers'
+          )
+        },
+        {
+          from: path.join(
+            __dirname,
+            'node_modules',
+            'itk',
+            'PolyDataIOs',
+            'VTKXMLFileReader.js'
+          ),
+          to: path.join(
+            __dirname,
+            'jupyterview',
+            'labextension',
+            'static',
+            'itk',
+            'PolyDataIOs',
+            'VTKXMLFileReader.js'
+          )
+        },
+        {
+          from: path.join(
+            __dirname,
+            'node_modules',
+            'itk',
+            'PolyDataIOs',
+            'VTKXMLFileReaderWasm.js'
+          ),
+          to: path.join(
+            __dirname,
+            'jupyterview',
+            'labextension',
+            'static',
+            'itk',
+            'PolyDataIOs',
+            'VTKXMLFileReaderWasm.js'
+          )
+        },
+        {
+          from: path.join(
+            __dirname,
+            'node_modules',
+            'itk',
+            'PolyDataIOs',
+            'VTKXMLFileReaderWasm.wasm'
+          ),
+          to: path.join(
+            __dirname,
+            'jupyterview',
+            'labextension',
+            'static',
+            'itk',
+            'PolyDataIOs',
+            'VTKXMLFileReaderWasm.wasm'
+          )
+        }
+      ]
+    })
+  ],
+  performance: {
+    maxAssetSize: 10000000
+  }
+};
